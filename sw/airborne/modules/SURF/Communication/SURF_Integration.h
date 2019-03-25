7c1f27956835e9d9ea898bed0d4e3278d2b435ed
Jibbam <jipbuwalda@hotmail.com>
2019-03-25 11:02:04 +0100

    communication project (to be done)

---
+++ sw/airborne/modules/SURF/Communication/SURF_Integration.h
@@ -0,0 +1,14 @@
+#ifndef SURF_INTEGRATION_H
+#define SURF_INTEGRATION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+vector<KeyPoint> surfGetImageObjects(char *prevImgs, int width, int heigth, char *newImg);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
