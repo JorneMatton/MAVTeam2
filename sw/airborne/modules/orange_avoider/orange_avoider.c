5ab3c48a1ac335bb2d5628405827abec16e82981
Jibbam <jipbuwalda@hotmail.com>
2019-03-28 17:31:38 +0100

    delete orange avoider

--- sw/airborne/modules/orange_avoider/orange_avoider.c
+++ sw/airborne/modules/orange_avoider/orange_avoider.c
@@ -35,30 +35,6 @@
 #define PRINT(string,...) fprintf(stderr, "[orange_avoider->%s()] " string,__FUNCTION__ , ##__VA_ARGS__)
 #define VERBOSE_PRINT PRINT
 
-#ifndef ORANGE_AVOIDER_LUM_MIN
-#define ORANGE_AVOIDER_LUM_MIN 41
-#endif
-
-#ifndef ORANGE_AVOIDER_LUM_MAX
-#define ORANGE_AVOIDER_LUM_MAX 183
-#endif
-
-#ifndef ORANGE_AVOIDER_CB_MIN
-#define ORANGE_AVOIDER_CB_MIN 53
-#endif
-
-#ifndef ORANGE_AVOIDER_CB_MAX
-#define ORANGE_AVOIDER_CB_MAX 121
-#endif
-
-#ifndef ORANGE_AVOIDER_CR_MIN
-#define ORANGE_AVOIDER_CR_MIN 134
-#endif
-
-#ifndef ORANGE_AVOIDER_CR_MAX
-#define ORANGE_AVOIDER_CR_MAX 249
-#endif
-
 
 uint8_t moveWaypointForward(uint8_t waypoint, float distanceMeters);
 uint8_t moveWaypointSideways(uint8_t waypoint);
@@ -76,7 +52,6 @@ enum navigation_state_t {
 
 // define settings
 
-float oa_color_count_frac = 0.15f;
 
 
 
@@ -112,21 +87,6 @@ zone_right = zone3;
 
 
 
-#ifndef ORANGE_AVOIDER_VISUAL_DETECTION_ID
-#define ORANGE_AVOIDER_VISUAL_DETECTION_ID ABI_BROADCAST
-#endif
-static abi_event color_detection_ev;
-static void color_detection_cb(uint8_t __attribute__((unused)) sender_id,
-                               int16_t __attribute__((unused)) pixel_x, int16_t __attribute__((unused)) pixel_y,
-                               int16_t __attribute__((unused)) pixel_width, int16_t __attribute__((unused)) pixel_height,
-                               int32_t quality, int16_t __attribute__((unused)) extra)
-{
-  color_count = quality;
-}
-
-
-
-
 
 static void send_orange_avoider_data(struct transport_tx *trans , struct link_device *dev){
   uint16 a = (uint16)zone_left, b = (uint16)zone_middle,c = (uint16)zone_right, d = (uint16)obstacle_free_confidence, e = (uint16)navigation_state;
@@ -144,7 +104,6 @@ void orange_avoider_init(void)
 
   // bind our colorfilter and SURF algorithm callbacks to receive the color filter and SURF outputs
   AbiBindMsgSURF_OBSTACLE(ORANGE_AVOIDER_SURF_OBSTACLE_ID, &surf_detection_ev, surf_detection_cb);
-  AbiBindMsgVISUAL_DETECTION(ORANGE_AVOIDER_VISUAL_DETECTION_ID, &color_detection_ev, color_detection_cb);
   register_periodic_telemetry(DefaultPeriodic,PPRZ_MSG_ID_ORANGE_AVOIDER,send_orange_avoider_data);
 }
 
@@ -158,11 +117,9 @@ void orange_avoider_periodic(void)
     return;
   }
 
-  int32_t color_count_threshold = oa_color_count_frac * front_camera.output_size.w * front_camera.output_size.h;
-
-  VERBOSE_PRINT("color count: %d, zone1: %d, zone2: %d, zone3: %d,  obstacle free confidence: %d state: %d \n", color_count, zone_left, zone_middle, zone_right, obstacle_free_confidence, navigation_state);
+  VERBOSE_PRINT("zone1: %d, zone2: %d, zone3: %d,  obstacle free confidence: %d state: %d \n", zone_left, zone_middle, zone_right, obstacle_free_confidence, navigation_state);
   // update our safe confidence using heading_target input (from SURF feature detection)
-  if(zone_middle<treshold_middle || color_count < color_count_threshold){
+  if(zone_middle<treshold_middle){
     obstacle_free_confidence++;
   } else {
     obstacle_free_confidence -= 1;  // be more cautious with positive obstacle detections
